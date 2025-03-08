22ddeba4dfb63a512dfa3a6dced7a6da233c7faa
Mahmoud822004 <mahmoudhisham822004@gmail.com>
2025-03-09 01:10:14 +0200

    data

--- src/project_task_status/dto/create-project_task_status.dto.ts
+++ src/project_task_status/dto/create-project_task_status.dto.ts
@@ -1,15 +1,16 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsEnum, IsNotEmpty, IsNumber } from "class-validator";
 import { TaskStatus } from "../entities/project_task_status.entity";
+import { Project } from "src/project/entities/project.entity";
 
 export class CreateProjectTaskStatusDto {
     @IsNumber()
     @IsNotEmpty()
     @ApiProperty({ example: 1, description: "ID of the project" })
-    project_id: number;
+    project_id: Project;
 
     @IsEnum(TaskStatus)
     @IsNotEmpty()
-    @ApiProperty({ example: TaskStatus.ToDo, description: "Status of the project task", enum: TaskStatus })
+    @ApiProperty({ example: TaskStatus.Pending, description: "Status of the project task", enum: TaskStatus })
     status: TaskStatus;
 }
