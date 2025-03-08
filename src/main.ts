b11038af8aa04c8041915898e3a0273e1927b025
unknown <mohamedhalemm40@gmail.com>
2025-03-09 00:42:29 +0200

    new-relation

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
    .setVersion('1.0')
    .addBearerAuth()
    .build();
-
+  
    const document = SwaggerModule.createDocument(app, config);
    SwaggerModule.setup('api', app, document);
    
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,6 +1,6 @@
 import { Department } from "src/departments/entities/department.entity";
 import { ProjectUser } from "src/project_user/entities/project_user.entity";
-import { Column, CreateDateColumn, DeleteDateColumn, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 export enum UserGender{
     Male = 'male',
@@ -49,7 +49,7 @@ export class User {
     @DeleteDateColumn()
     deletedAt: Date;
 
-    @ManyToOne(()=> ProjectUser, projectUser => projectUser.user_id)
+    @OneToMany(()=> ProjectUser, projectUser => projectUser.user_id)
     projectUsers: ProjectUser[];
 
     
